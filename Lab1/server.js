91e4a0e7ded025cf364281e6a16ae9608d6ea8dc
Simranpreet Kaur <simarkaur3320@gmail.com>
2020-09-25 23:27:11 -0400

    Commit1

--- Lab1/server.js
+++ Lab1/server.js
@@ -4,5 +4,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World  \nI am Simranpreet Kaur');
+    res.end('Hello World  \nI am Simranpreet Kaur(200427339)');
 }).listen(port);
